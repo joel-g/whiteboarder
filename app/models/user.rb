1c3589cb7324540d8555cb1743467f171d9fade7
Joel Guerra <joelaguerra1@gmail.com>
2017-07-07 16:53:08 -0700

    adds user authentication to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,32 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+
+validates :email, presence: true, uniqueness: true
+validates :username, presence: true, uniqueness: true
+validates :hashed_password, presence: true
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate(password)
+    self.password == password
+  end
+
+  def self.authenticate(username, password)
+    user = self.find_by(username: username)
+    if user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+
 end
