00abc549da17b5bd0d27e8000d428cc3b04d8431
Joel Guerra <joelaguerra1@gmail.com>
2017-07-13 14:14:28 -0700

    builds interview controller

---
+++ app/controllers/interview_controller.rb
@@ -0,0 +1,27 @@
+get '/interviews/new' do
+  if session[:id]
+    pass_user
+    erb :'interviews/new'
+  else
+    redirect '/sessions/new'
+  end
+end
+
+post '/interviews' do
+  pass_user
+  int = Interview.new(params[:int])
+  if int.save
+    redirect "/interviews/#{int.id}"
+  else
+    @error = 'All fields are required.'
+    erb :'interviews/new'
+  end
+end
+
+get '/interviews/:id' do
+  if session[:id]
+    pass_user
+  end
+  @interview = Interviews.find(params[:id])
+  erb :'interviews/show'
+end
