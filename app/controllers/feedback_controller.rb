f26a2c817a6e893e2370e444eae16d91ed8fcabb
joel-g <joelaguerra1@gmail.com>
2017-07-14 07:37:58 -0700

    fills out feedback post route

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,12 +1,11 @@
 post '/interviews/:inteview_id/feedbacks' do
-  review = Feedback.new(params[:feed])
+  feedback = Feedback.new(params[:feed])
   @interview = Interview.find(params[:feed][:interview_id])
   pass_user
-  if review.save
-    # @success = 'Thank you for your review!'
-    # erb :'interview/show'
+  if feedback.save
+    redirect "/interviews/#{@interview.id}"
   else
-    # @error = 'all fields are required'
-    # erb :'restaurants/show'
+    @error = 'all fields are required'
+    erb :'interviews/show'
   end
 end
