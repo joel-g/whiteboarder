b791515a4c564d71c7320333e002834762504153
joel-g <joelaguerra1@gmail.com>
2017-07-14 07:54:24 -0700

    builds out challenge selection, updates challenges migration

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,5 @@
 get '/' do
   pass_user
+  @challenges = Challenge.all
   erb :'index'
 end
--- db/migrate/20170707162343_create_challenges.rb
+++ db/migrate/20170707162343_create_challenges.rb
@@ -1,7 +1,8 @@
 class CreateChallenges < ActiveRecord::Migration
   def change
     create_table :challenges do |t|
-      t.string :question, null: false
+      t.string :title, null: false
+      t.string :body, null: false
       t.string :solution
       t.integer :difficulty
       t.integer :user_id, null: false
